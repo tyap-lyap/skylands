69ad274846ccfaac8fa3c13f760077585bc8f89f
PinkGoosik <pinkgoosik@vk.com>
2022-08-08 20:41:33 +0500

    protect fence gates

---
+++ src/main/java/skylands/mixin/FenceGateBlockMixin.java
@@ -0,0 +1,36 @@
+package skylands.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FenceGateBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import skylands.Mod;
+import skylands.logic.Skylands;
+
+import java.util.UUID;
+
+@Mixin(FenceGateBlock.class)
+public abstract class FenceGateBlockMixin {
+
+	@Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
+	void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+		if(!world.isClient) {
+			if(world.getRegistryKey().getValue().getNamespace().equals(Mod.MOD_ID)) {
+				var island = Skylands.instance.islandStuck.get(UUID.fromString(world.getRegistryKey().getValue().getPath()));
+				if(island.isPresent() && !island.get().isMember(player)) {
+					player.sendMessage(Text.of("Skylands > You can't interact with fence gates out here!"), true);
+					cir.setReturnValue(ActionResult.FAIL);
+				}
+			}
+		}
+	}
+}

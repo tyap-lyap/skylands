d48d17e163e9316279c65e8adfb83a278b63dedc
PinkGoosik <pinkgoosik@vk.com>
2022-08-16 10:06:59 +0500

    add custom motd

--- src/main/java/skylands/event/ServerStartEvent.java
+++ src/main/java/skylands/event/ServerStartEvent.java
@@ -1,11 +1,23 @@
 package skylands.event;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
 import skylands.logic.Skylands;
 
 public class ServerStartEvent {
 
 	public static void onStart(MinecraftServer server) {
 		Skylands.instance = new Skylands(server);
+
+		var serverMeta = server.getServerMetadata();
+		var motd = serverMeta.getDescription();
+
+		if(motd == null || motd.getString().equals("A Minecraft Server")) {
+			FabricLoader.getInstance().getModContainer("skylands").ifPresent(mod -> {
+				var modMeta = mod.getMetadata();
+				serverMeta.setDescription(Text.of("Skylands Alpha Build " + modMeta.getVersion().getFriendlyString()));
+			});
+		}
 	}
 }

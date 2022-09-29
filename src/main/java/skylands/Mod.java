ef644a50b6f8dc5077b809b77fecc7fa420a2b5e
PinkGoosik <pinkgoosik@vk.com>
2022-09-29 15:58:10 +0500

    fix crash probably

--- src/main/java/skylands/Mod.java
+++ src/main/java/skylands/Mod.java
@@ -1,8 +1,6 @@
 package skylands;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,11 +17,6 @@ public class Mod implements ModInitializer {
 		ModEvents.init();
 		ModCommands.init();
 		SongsData.init();
-		//Example code, you can remove it
-		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(mod -> {
-			ModMetadata meta = mod.getMetadata();
-			LOGGER.info(meta.getName() + " " + meta.getVersion().getFriendlyString() + " successfully initialized!");
-		});
 	}
 
 	public static Identifier id(String path) {

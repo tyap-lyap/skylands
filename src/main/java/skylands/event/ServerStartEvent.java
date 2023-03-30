3fcdf9052269bb5d2287166874685104aef468e9
PinkGoosik <MCGycuk@yandex.ru>
2023-03-30 22:26:51 +0500

    fix datapacks from hub template not loading on first join

--- src/main/java/skylands/event/ServerStartEvent.java
+++ src/main/java/skylands/event/ServerStartEvent.java
@@ -1,13 +1,17 @@
 package skylands.event;
 
+import com.google.common.collect.Lists;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.WorldSavePath;
+import net.minecraft.world.SaveProperties;
 import org.apache.commons.io.FileUtils;
 import skylands.SkylandsMod;
 import skylands.logic.Skylands;
 
 import java.io.File;
+import java.util.Collection;
 
 public class ServerStartEvent {
 
@@ -24,6 +28,8 @@ public class ServerStartEvent {
 				if(hubTemplate.exists() && !lock.exists()) {
 					FileUtils.copyDirectory(hubTemplate, new File(path));
 					lock.createNewFile();
+					SkylandsMod.LOGGER.info("Reloading datapacks from hub template...");
+					reloadDatapacks(server);
 				}
 			}
 		}
@@ -40,5 +46,27 @@ public class ServerStartEvent {
 				server.setMotd("Skylands Beta v" + modMeta.getVersion().getFriendlyString());
 			});
 		}
+	}
+
+	private static void reloadDatapacks(MinecraftServer server) {
+		ResourcePackManager resourcePackManager = server.getDataPackManager();
+		SaveProperties saveProperties = server.getSaveProperties();
+		Collection<String> collection = resourcePackManager.getEnabledNames();
+		Collection<String> dataPacks = findNewDataPacks(resourcePackManager, saveProperties, collection);
+		server.reloadResources(dataPacks).exceptionally(throwable -> null);
+	}
+
+	private static Collection<String> findNewDataPacks(ResourcePackManager dataPackManager, SaveProperties saveProperties, Collection<String> enabledDataPacks) {
+		dataPackManager.scanPacks();
+		Collection<String> collection = Lists.newArrayList(enabledDataPacks);
+		Collection<String> collection2 = saveProperties.getDataConfiguration().dataPacks().getDisabled();
+
+		for(String string : dataPackManager.getNames()) {
+			if (!collection2.contains(string) && !collection.contains(string)) {
+				collection.add(string);
+			}
+		}
+
+		return collection;
 	}
 }
